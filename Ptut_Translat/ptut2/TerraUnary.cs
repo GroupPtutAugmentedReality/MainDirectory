68c82354c497e0b471e89774b6fa5a3fb7bbe118
TheScreamer74 <blakilleur@gmail.com>
2018-12-19 17:02:43 +0100

    17:02 terraunary

--- Ptut_Translat/ptut2/TerraUnary.cs
+++ Ptut_Translat/ptut2/TerraUnary.cs
@@ -1,11 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace ptut2
 {
     class TerraUnary : TerraNode
     {
-        TerraNode node
-    }
+		public TerraNode node;
+
+		TerraUnary(TerraNode n)
+		{
+			node = n;
+		}
+
+		/*public TerraNode PutAt(TerraNode node, ref Vector s, ref double a, ref Vector2 t)
+		{
+		  return new TerraTranslate(new TerraRotate(new TerraScale(node, s), a), t);
+		}*/
+
+	}
 }
